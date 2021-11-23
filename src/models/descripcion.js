6d3e091cbe5b9428f367bbd8e39b2c33231760cb
OscarTalero <talero@gmail.com>
2021-11-23 08:15:14 -0500

    limpiar archivos

--- src/models/descripcion.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DescripcionSchema = new mongoose.Schema({
-    area: Number,
-    habitaciones: Number,
-    plantas: Number,
-});
-
-module.exports = mongoose.model('Descripcion', DescripcionSchema);
--- src/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    nombre: String,
-    permiso: String
-});
-
-module.exports = mongoose.model('User', UserSchema);
